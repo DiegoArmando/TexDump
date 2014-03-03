0f2eac724ac69aab1a21c33da11afea9c7fcdadc
wjuffras <wjuffras@gmail.com>
2014-03-03 10:31:41 -0500

    Updated with Comments
    Server procedure:
    -find address info of local machine
    -create socket
    -bind to a local port
    -set port to listen for connections
    -wait to accept client connections
    -receive and display text data from client
    
    Client procedure:
    -get address info of server
    -connect to server's socket
    -send text data to server

--- C++/Communication/Client/Client/Source.cpp
+++ C++/Communication/Client/Client/Source.cpp
@@ -6,6 +6,7 @@
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <iostream>
+#include <string>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -16,7 +17,7 @@ int main()
 
 	int iResult;
 
-	// Initialize Winsock
+	// Initialize Winsock2
 	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (iResult != 0)
 	{
@@ -24,18 +25,17 @@ int main()
 		return 1;
 	}
 	struct addrinfo *result = NULL,
-		*ptr = NULL,
 		hints;
 
 	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_family = AF_INET;//IP version 4
+	hints.ai_socktype = SOCK_STREAM;//Work like a pipe
+	hints.ai_protocol = IPPROTO_TCP;//TCP protocol
 
 	#define DEFAULT_PORT "27015"
 
-	// Resolve the server address and port
-	iResult = getaddrinfo("129.161.204.194", DEFAULT_PORT, &hints, &result);
+	// Resolve the server address and port. IP address is of the server
+	iResult = getaddrinfo("129.161.205.96", DEFAULT_PORT, &hints, &result);
 	if (iResult != 0) {
 		printf("getaddrinfo failed: %d\n", iResult);
 		WSACleanup();
@@ -45,35 +45,27 @@ int main()
 
 	SOCKET ConnectSocket = INVALID_SOCKET;
 
-	ptr = result;
-
-	// Create a SOCKET for connecting to server
-	ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,ptr->ai_protocol);
+	// Create a SOCKET for connecting to server with 
+	ConnectSocket = socket(result->ai_family, result->ai_socktype,result->ai_protocol);
 	if (ConnectSocket == INVALID_SOCKET) {
 		printf("Error at socket(): %ld\n", WSAGetLastError());
 		freeaddrinfo(result);
 		WSACleanup();
 		return 1;
 	}
-	iResult = connect(ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+	//Connect to server's socket. The servers socket is binded to a socket address 
+	//with the configurations that the addrinfo result gathered.
+	iResult = connect(ConnectSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR) {
 		closesocket(ConnectSocket);
-		ConnectSocket = INVALID_SOCKET;
-	}
-
-	// Should really try the next address returned by getaddrinfo
-	// if the connect call failed
-	// But for this simple example we just free the resources
-	// returned by getaddrinfo and print an error message
-
-	freeaddrinfo(result);
-
-	if (ConnectSocket == INVALID_SOCKET) {
 		printf("Unable to connect to server!\n");
 		WSACleanup();
 		return 1;
 	}
+	// Should really try the next address returned by getaddrinfo
+	// if the connect call failed
 
+	freeaddrinfo(result);//delete result and free memory
 
 	#define DEFAULT_BUFLEN 512
 	char *sendbuf = "this is a test";
@@ -88,9 +80,9 @@ int main()
 	}
 	while (true)
 	{
-		char buffer [DEFAULT_BUFLEN];
-		std::cin >> buffer;
-		iResult = send(ConnectSocket, buffer, (int)strlen(buffer), 0);
+		std::string temp;
+		getline(std::cin,temp);//gather new line from user input
+		iResult = send(ConnectSocket, temp.c_str(), temp.length(), 0);//send user input to server
 		if (iResult == SOCKET_ERROR) {
 			printf("send failed: %d\n", WSAGetLastError());
 			closesocket(ConnectSocket);
--- C++/Communication/Server/Server/Source.cpp
+++ C++/Communication/Server/Server/Source.cpp
@@ -8,7 +8,10 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-
+/*This function takes in a sockaddr, determines weather it is IPv4 of IPv6 and then returnes 
+  an IN_ADDR(IP address container) with the IP address in the sockaddr. This is currently used to
+  display the IP address to the console.
+ */
 void *get_in_addr(struct sockaddr *sa)
 {
 	if (sa->sa_family == AF_INET) {
@@ -21,8 +24,8 @@ void *get_in_addr(struct sockaddr *sa)
 
 int main() 
 {
-	char ipstr[INET_ADDRSTRLEN];
-	WSADATA wsaData;
+	char ipstr[INET_ADDRSTRLEN];//This will contain the char* of the IP address in readable format
+	WSADATA wsaData;//This is used to initialize Winsock
 
 	int iResult;
 
@@ -34,37 +37,47 @@ int main()
 		return 1;
 	}
 
-	#define DEFAULT_PORT "27015"
+	#define DEFAULT_PORT "27015"//This is the port that the server will bind and listen to.
 
 	struct addrinfo *result = NULL, *ptr = NULL, hints;
 
-	ZeroMemory(&hints, sizeof (hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
+	/* In this block of code determine the addrinfo so that the server can create a socket that
+	will bind and listen to this particular address
+	*/
+	ZeroMemory(&hints, sizeof (hints));//Makes all values in hints 0 
+	hints.ai_family = AF_INET;//IPv4
+	hints.ai_socktype = SOCK_STREAM;//Communication works similar to a pipe
+	hints.ai_protocol = IPPROTO_TCP;//TCP protocol
+	hints.ai_flags = AI_PASSIVE;//Socket address is capable of binding and listening
 	char hostname[128];
-	gethostname(hostname, 128);
-	// Resolve the local address and port to be used by the server
-	iResult = getaddrinfo(hostname, DEFAULT_PORT, &hints, &result);
+	gethostname(hostname, 128);//retrieves local host name and sets it to hostname
+	//Using the hints, figure out the full information of the socket address
+	iResult = getaddrinfo(hostname, DEFAULT_PORT, &hints, &result);//function returns number of addresses that match requirements
 	if (iResult != 0) {
 		printf("getaddrinfo failed: %d\n", iResult);
 		WSACleanup();
 		return 1;
 	}
+
+
+	//set ipstr to the ip address of the address in the addrinfo variable result.
 	inet_ntop(result->ai_family, get_in_addr((struct sockaddr *)result->ai_addr), ipstr, sizeof ipstr);
 	printf("Server's IP: ");
 	printf(ipstr);
 	printf("\n\r");
-	SOCKET ListenSocket = INVALID_SOCKET;
+
+
+	SOCKET ListenSocket = INVALID_SOCKET;//Declare the socket
+	//Initalize socket using the result's characteristics
 	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-	if (ListenSocket == INVALID_SOCKET) {
+	if (ListenSocket == INVALID_SOCKET) 
+	{
 		printf("Error at socket(): %ld\n", WSAGetLastError());
 		freeaddrinfo(result);
 		WSACleanup();
 		return 1;
 	}
-
+	//Bind socket to the address in result
 	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
 	if (iResult == SOCKET_ERROR) {
 		printf("bind failed with error: %d\n", WSAGetLastError());
@@ -73,7 +86,7 @@ int main()
 		WSACleanup();
 		return 1;
 	}
-	
+	//configure socket to be listening for attemted connections. SOMAXCONN maximum number of connections.
 	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
 		printf("Listen failed with error: %ld\n", WSAGetLastError());
 		closesocket(ListenSocket);
@@ -81,18 +94,20 @@ int main()
 		return 1;
 	}
 
-	SOCKET ClientSock = SOCKET_ERROR;
-	SOCKADDR_IN client_info = { 0 };
+	SOCKET ClientSock = SOCKET_ERROR;//Declare a Client Socket
+	SOCKADDR_IN client_info = { 0 };//Socket info of client 
 	int addrsize = sizeof(client_info);
 
 
-	while (ClientSock == SOCKET_ERROR)
+	while (ClientSock == SOCKET_ERROR)//Never stop trying to connect to a client
 	{
-		ClientSock = accept(ListenSocket, (struct sockaddr*)&client_info, NULL);
+		//Accept a connection implemented by a client
+		ClientSock = accept(ListenSocket, NULL, NULL);
 	}
+	//gets the sockaddr infomation of the clients socket
 	getpeername(ClientSock, (struct sockaddr*)&client_info, &addrsize);
 
-	char *ip = inet_ntoa(client_info.sin_addr);
+	char *ip = inet_ntoa(client_info.sin_addr);//Only works for IPv4
 
 	printf("Connected to: %s\r\n", ip);
 
@@ -104,7 +119,10 @@ int main()
 
 	// Receive until the peer shuts down the connection
 	do {
-
+		//The code waits until something is recieved accross the client socket.
+		//Stores recieved data in recvbuf and buffer lenght in recvbuflen
+		//when a connection is closed an empty packet is sent to the server
+		//iResult should be equivelent to recvbuflen
 		iResult = recv(ClientSock, recvbuf, recvbuflen, 0);
 		if (iResult > 0) 
 		{
